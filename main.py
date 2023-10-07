0f5553e03a0f2b393fd9f3aef88c428f295d86af
jazir788 <jazirsx5@gmail.com>
2023-10-07 11:57:46 +0100

    ToDo List - Python Learning, Day 4.
    Experimenting with list indexing and tuples

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 todosList = []
 
 while True:
-    user_action = input("Type add or show/display or exit: ").strip()
+    user_action = input("Type add, show/display, edit  or exit: ").strip()
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todosList.append(todo)
+        case 'edit':
+            print(todosList)
+            number = int(input('Number of the todo to edit: '))
+            editedItem = input('What is the new todo item')
+            number = number-1
+            todosList.__setitem__(number, editedItem)
+            print(todosList)
         case 'show' | 'display':
             for item in todosList:
-                listLength = len(todosList)
+                print(item)
         case 'exit':
             break
+
         case _:
             print('You entered an unknown command')
