e63728e285d699ae728d38da49cc12b00376dba8
jazir788 <jazirsx5@gmail.com>
2023-10-07 13:21:22 +0100

    ToDo List - Python Learning, Day 5.
    Experimenting with enumeration and f-strings

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 todosList = []
 while True:
-    user_action = input("Type add, show/display, edit  or exit: ").strip()
+    user_action = input("Type add, show/display, edit, complete  or exit: ").strip()
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todosList.append(todo)
+        case 'complete':
+            print(todosList)
+            number = int(input("Enter the todo item that is complete: "))
+            todosList.pop(number-1)
+            print(todosList)
         case 'edit':
             print(todosList)
             number = int(input('Number of the todo to edit: '))
@@ -13,8 +18,9 @@ while True:
             todosList.__setitem__(number, editedItem)
             print(todosList)
         case 'show' | 'display':
-            for item in todosList:
-                print(item)
+            for index, item in enumerate(todosList):
+                row = f"{index+1}-{item}"
+                print(row)
         case 'exit':
             break
         case _:
