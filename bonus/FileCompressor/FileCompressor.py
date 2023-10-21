f70f4f4a06141e2f3eb0fafbff1cbd33fc23e0ef
jazir788 <jazirsx5@gmail.com>
2023-10-21 22:11:55 +0100

    ToDo List - Python Learning, Created File Compressor app

---
+++ bonus/FileCompressor/FileCompressor.py
@@ -0,0 +1,27 @@
+import PySimpleGUI as sg
+import zip_creator as zip
+
+
+label1 = sg.Text("Select files to compress:")
+input1 = sg.Input()
+choose_button1 = sg.FileBrowse("Choose", key= "files")
+
+label2 = sg.Text("Select destination Folder")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
+
+compress_button = sg.Button("Compress")
+output_label = sg.Text(key="output", text_color="green")
+window = sg.Window("File Compressor", layout=[[label1, input1, choose_button1],
+                                              [label2, input2, choose_button2],
+                                              [compress_button, output_label],])
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(",")
+    folder = values["folder"]
+    zip.make_archive(filepaths, folder)
+    window["output"].update(value = "Compression Completed")
+
+
+window.close()
