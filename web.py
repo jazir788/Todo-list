b9f614cb8da44d0cc05ec2408811999217df1694
jazir788 <jazirsx5@gmail.com>
2023-10-24 14:57:23 +0100

    ToDo List - Python Learning,Created Web App using Streamlit

---
+++ web.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from modules import functions
+
+todos = functions.get_todos()
+
+st.title("My Todo App")
+st.subheader("This is the best todo app")
+st.write("This app is to increase your productivity")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="Enter a todo", placeholder="Add a todo..")
+
+
+
