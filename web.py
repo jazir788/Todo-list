3a4b21dafe9489a961894a16a29bd888fd76f33a
jazir788 <jazirsx5@gmail.com>
2023-10-24 15:10:12 +0100

    ToDo List - Python Learning,Added add todo item functionality to web app

--- web.py
+++ web.py
@@ -3,6 +3,13 @@ from modules import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+
 st.title("My Todo App")
 st.subheader("This is the best todo app")
 st.write("This app is to increase your productivity")
@@ -10,7 +17,8 @@ st.write("This app is to increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo", placeholder="Add a todo..")
-
+st.text_input(label="Todo", placeholder="Add a todo..",
+              on_change=add_todo, key='new_todo')
+st.session_state
 
 
